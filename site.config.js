907bbbfaa897b9d0efb36a5f83551dee2cabea57
MARCELXV <67491786+marcelxv@users.noreply.github.com>
2021-08-17 13:44:54 -0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '4b4f11b6bddb48e79a8768ecc8e02274',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Marcel The Creator',
   domain: 'transitivebullsh.it',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
   description: 'Example site description',
   socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  socialImageSubtitle: 'Hello Mundo! 👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
